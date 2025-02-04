fb6b4f4b0a5fd1c970c3fd4214ae754e1b21aee8
jasonpellegrini <jpellegrini1616@gmail.com>
2025-02-04 15:20:38 -0500

    Navbar fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import lamppostIcon from '../assets/lamppost.png';
 const Navbar = () => {
     return (
         <nav className="bg-[#131313] text-white w-screen h-16 flex items-center px-6">
-            <div className="max-w-screen-xl flex items-center w-full">
-                <div className="flex items-center space-x-4 logo-container">
+            <div className="w-full flex items-center justify-between">
+                <div className="flex items-center logo-container space-x-4">
                     <img
                         src={lamppostIcon}
                         alt="Welcome to LampPost!"
@@ -16,10 +16,6 @@ const Navbar = () => {
                     </h1>
                 </div>
 
-                {/* <ul className="flex space-x-6 text-lg ml-auto">
-                    <li><h2 className="hover:text-[#ED2938]">Home</h2></li>
-                    <li><h2 className="hover:text-[#ED2938]">Stats</h2></li>
-                </ul> */}
                 <ul className="flex space-x-6 text-lg ml-auto">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/stats">Stats</Link></li>

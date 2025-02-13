c5102791e29f43b3faf52a2e4d0f2e2176d7a2f2
BonkAtomicPunch1 <167843785+BonkAtomicPunch1@users.noreply.github.com>
2025-02-13 01:59:45 -0500

    Rewritten
    rewrote Css to Tailwind

--- frontend/src/pages/Stats.jsx
+++ frontend/src/pages/Stats.jsx
@@ -3,28 +3,41 @@ import Bar_Census from '../assets/Bar_Census.jpeg';
 
 const Stats = () => {
     return (
-    <body>
-      <div className="right-float-container">
-        <h1 className= "graph-title">Graph of Data</h1>
-        <img src={Bar_Census} alt="Bar Census" className="right-float-image" />
-        <h1 className= "graph-title">Map of Area?</h1>
-        <img src = 'blank' alt = "Future heatmap?" className="right-float-image" />
-      </div>
-      <div className='left-float-container'>
-        <h1 className= "breakdown-title">Crimes by popularity</h1>
-          <p>this is a area set aside for data in the future</p>
-          <p>it will probably be going down the page like this</p>
-          <p>it will probably be hard to get the data to work properly</p>
-          <p>but with our group im sure its possible!</p>
-        <h1 className= "breakdown-title">Most Recent Crimes</h1>
-          <p>this is a area set aside for data in the future</p>
-          <p>it will probably be going down the page like this</p>
-          <p>it will probably be hard to get the data to work properly</p>
-          <p>but with our group im sure its possible!</p>
-      </div>
-      <div>
+    <body className='bg [#333333] flex flex-col'>
+      <div className="bg-[#333333] flex w-screen justify-evenly h-60%">
+        <div className ="bg-[#333333] flex flex-col w-50%">
+          <div className = 'flex flex-col justify-center'>
+            <h1 className = "text-2xl text-[#FF8C01]">Graph of Data</h1>
+            <img className = "w-50% h-50%" src={Bar_Census} alt="Bar Census" />
+          </div>
+          <div className = 'flex flex-col justify-center'>
+            <h1 className = "text-2xl text-[#FF8C01]"> Map of Area?</h1>
+            <img className = "w-50% h-50%" src = {Bar_Census} alt = "Future heatmap?" />
+          </div>
+        </div>
+        
+        <div className="bg-[#333333] flex flex-col w-50% justify-evenly justify-evenly content-evenly">
+          <div className = "m-3 align-center bg-[#C62C2C] ">
+            <ul className = "text-2xl text-[#FF8C01]" >Crimes by popularity</ul>
+              <li className = "align-center text">this is a area set aside for data in the future</li>
+              <li className = "align-center">it will probably be going down the page like this</li>
+              <li className = "align-center">it will probably be hard to get the data to work properly</li>
+              <li className = "align-center">but with our group im sure its possible!</li>
+          </div>
+          <div className = "m-3 bg-[#C62C2C]">
+            <ul className = "text-2xl text-[#FF8C01]" >Most Recent Crimes</ul>
+              <li className = "align-center">this is a area set aside for data in the future</li>
+              <li className = "align-center">it will probably be going down the page like this</li>
+              <li className = "align-center">it will probably be hard to get the data to work properly</li>
+              <li className = "align-center">but with our group im sure its possible!</li>
+          </div>
+        </div>
+      </div>  
+      <footer className = "bg-[#420D09] w-screen">
+        <p>this is in the footer</p>
+      </footer>
 
-      </div>
+      
     </body> 
     );
   };

cd7b5cc0535db11d89a2afc2070b4418ee4d469b
jasonpellegrini <jpellegrini1616@gmail.com>
2025-02-13 13:24:53 -0500

    savepoint

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -30,9 +30,9 @@ const Map = () => {
                 url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             />
-            <Marker icon={customIcon} position={[51.505, -0.09]}>
+            <Marker icon={customIcon} position={[40.6215, -79.1525]}>
                 <Popup>
-                Test popup
+                <h1>Test popup!</h1>
                 </Popup>
             </Marker>
         </MapContainer>

0a1822f927f625513334f21d489d5a3b9ee755ef
Nathan Metzger <nathan@alphapuggle.dev>
2025-05-02 16:36:37 -0400

    Consolidate geoid upload into one python file

--- geoid_final_table.py
+++ geoid_final_table.py
@@ -1,12 +1,21 @@
 import geopandas as gpd
 import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv, dotenv_values
 
+load_dotenv()
+
+postgres_url = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(os.getenv("PG_USER",'postgres'),os.getenv("PG_PASSWORD",''),os.getenv("PG_HOST",'localhost'),os.getenv("PG_PORT","5432"),os.getenv("PG_DBNAME","postgres"))
+engine = create_engine(postgres_url)
+connection = engine.connect()
 # === Load shapefiles ===
-blocks = gpd.read_file(r'C:\Users\alecc\Downloads\tl_2024_42_tabblock20\tl_2024_42_tabblock20.shp')
-counties = gpd.read_file(r'C:\Users\alecc\Downloads\tl_2024_us_county\tl_2024_us_county.shp')
+blocks = gpd.read_file(r'./tl_2024_42_tabblock20/tl_2024_42_tabblock20.shp')
+counties = gpd.read_file(r'./tl_2024_us_county/tl_2024_us_county.shp')
 
 # === Load CSV with GEOID, OffenseType, ReportedOn ===
-input_df = pd.read_csv(r'C:\Users\alecc\Desktop\Rust\db_to_csv\ucr_crimes.csv')
+input_df = pd.read_sql_query("SELECT * FROM ucr_crime_data.ucr_crime_data;",con=connection)
 input_df['GEOID'] = input_df['GEOID'].astype(str)  # Ensure GEOID is string
 
 # === Prepare results ===
@@ -14,8 +23,8 @@ results = []
 
 for _, row in input_df.iterrows():
     geoid = str(row['GEOID']).zfill(15)
-    offense_type = row['Offense Type']
-    reported_on = row['Reported On']
+    offense_type = row['OffenseType']
+    reported_on = row['ReportedOn']
 
     block = blocks[blocks['GEOID20'] == geoid]
 
@@ -45,7 +54,6 @@ for _, row in input_df.iterrows():
     })
 
 # === Save final output ===
-output_df = pd.DataFrame(results)
-output_df.to_csv('ucr_final.csv', index=False)
+output_df = pd.DataFrame(results).to_sql('lamppost_data',engine,schema="ucr_crime_data",index=False,if_exists="replace")
 
-print("CSV saved as ucr_final.csv")
+print("Data stored in ucr_crime_data.lamppost_data")
